648492362f692cb925323a1925d5b3d7dc3e8fca
Tusharmahamud <30477458+Tusharmahamud@users.noreply.github.com>
2017-08-08 05:53:34 +0600

    change the data base name

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
 mysql_connect('localhost','root','') or die('cannot connect to the server');
-mysql_select_db('coderhousebd') or die('cannot select database');
+mysql_select_db('coder') or die('cannot select database');
 ?>
